80dcc7991f413d436a7a2ba966beb4a4b08b1e26
ZainRk <ahmadrao925@gmail.com>
2024-02-03 00:41:10 +0500

    have to work on the follow info box

--- sections/profile/FollowInfoBox.jsx
+++ sections/profile/FollowInfoBox.jsx
@@ -1,17 +1,30 @@
 import Box from "@/components/Box";
 import React from "react";
 import css from "@/styles/FollowInfoBox.module.css";
-import { Space, Typography } from "antd";
-const FollowInfoBox = ({ followers = [], followings = [] }) => {
+import { Alert, Skeleton, Space, Typography } from "antd";
+const FollowInfoBox = ({
+  isLoading,
+  isError,
+  followers = 0,
+  followings = 0,
+}) => {
+  if (isLoading)
+    return (
+      <Skeleton.Button active={true} size="large" style={{ width: "100%" }} />
+    );
+
+  if (isError)
+    return <Alert message="Error while fetching data" type="error" />;
+
   return (
     <Box className={css.container}>
       <Space direction="vertical" align="center">
-        <Typography className={"typoH5"}>{followers.length}</Typography>
+        <Typography className={"typoH5"}>{followers}</Typography>
         <Typography className={"typoSubtitle2"}>Followers</Typography>
       </Space>
 
       <Space direction="vertical" align="center">
-        <Typography className={"typoH5"}>{followings.length}</Typography>
+        <Typography className={"typoH5"}>{followings}</Typography>
         <Typography className={"typoSubtitle2"}>Followings</Typography>
       </Space>
     </Box>
