ece2bf2a3971ccf065a0af742eade0ffa3ce4947
ZainRk <ahmadrao925@gmail.com>
2024-02-03 10:59:50 +0500

    working on followers body

--- sections/profile/FollowersBody.jsx
+++ sections/profile/FollowersBody.jsx
@@ -1,17 +1,65 @@
 import React from "react";
 import css from "@/styles/FollowersBody.module.css";
-import { Typography } from "antd";
+import { Alert, Skeleton, Typography } from "antd";
 import UserBox from "@/components/UserBox";
-const FollowersBody = () => {
+import { useQuery } from "@tanstack/react-query";
+import { getAllFollowersAndFollowings } from "@/actions/user";
+import { useUser } from "@clerk/nextjs";
+const FollowersBody = (id) => {
+  const { user: currentUser } = useUser();
+
+  const {
+    data: userData,
+    isLoading: userDataLoading,
+    isError: userDataError,
+  } = useQuery({
+    queryKey: ["user", id, "followInfo"],
+    queryFn: () => getAllFollowersAndFollowings(id),
+    enabled: !!id,
+    // 20 mins stale time
+    staleTime: 1000 * 60 * 20,
+  });
+
+  const {
+    data: currentUserData,
+    currentUserLoading,
+    currentUserError,
+  } = useQuery({
+    queryKey: ["user", id, "followInfo"],
+    queryFn: () => getAllFollowersAndFollowings(currentUser?.id),
+    enabled: !!id,
+    // 20 mins stale time
+    staleTime: 1000 * 60 * 20,
+  });
+
+  if (userDataLoading)
+    return (
+      <Skeleton.Button
+        active={true}
+        size="large"
+        style={{ width: "100%", height: "3.5rem" }}
+      />
+    );
+
+  if (userDataError)
+    return <Alert message="Error while fetching data" type="error" />;
+
   return (
     <div className={css.container}>
       <div className={css.head}>
         <Typography className={"typoH5"}>Followers</Typography>
       </div>
       <div className={css.body}>
+        {/* <UserBox />
         <UserBox />
-        <UserBox />
-        <UserBox />
+        <UserBox /> */}
+        {userData?.followers?.map((person) => (
+          <UserBox
+            key={person.followerId}
+            data={person}
+            loggedInUserData={currentUserData}
+          />
+        ))}
       </div>
     </div>
   );
